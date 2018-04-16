e309a4ac535ed7034be576e1bc3fb06fc97df43a
Logan Schiessle <schiesla@rose-hulman.edu>
2018-04-16 17:48:14 -0400

    done

--- Photo Bucket Core Data/PictureDetailViewController.swift
+++ Photo Bucket Core Data/PictureDetailViewController.swift
@@ -11,26 +11,64 @@ import UIKit
 class PictureDetailViewController: UIViewController {
 
     var picture: Picture?
+    
+    @IBOutlet weak var photoCaptionLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.edit, target: self, action: #selector(showEditDialog))
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func showEditDialog() {
+        let alertController = UIAlertController(title: "Edit picture", message: "", preferredStyle: UIAlertControllerStyle.alert)
+    
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Caption"
+            textField.text = self.picture?.caption
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil)
+        
+        let createPhotoAction = UIAlertAction(title: "Edit", style: UIAlertActionStyle.default) { (action) in
+            let captionTextField = alertController.textFields![0]
+            self.picture?.caption = captionTextField.text!
+            self.updateView()
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+        }
+        
+        alertController.addAction(createPhotoAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func updateView() {
+        photoCaptionLabel.text = picture?.caption
     }
-    */
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if let imgString = picture?.pictureUrl {
+            if let imgUrl = URL(string: imgString) {
+                DispatchQueue.global().async { // Download in the background
+                    do {
+                        let data = try Data(contentsOf: imgUrl)
+                        DispatchQueue.main.async { // Then update on main thread
+                            self.photoImageView.image = UIImage(data: data)
+                        }
+                    } catch {
+                        print("Error downloading image: \(error)")
+                    }
+                }
+            }
+        }
+    }
+
 
 }
--- Photo Bucket Core Data/PictureTableViewController.swift
+++ Photo Bucket Core Data/PictureTableViewController.swift
@@ -20,12 +20,40 @@ class PictureTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        navigationItem.leftBarButtonItem = editButtonItem
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.add, target: self, action: #selector(showAddDialog))
+    }
+    
+    @objc func showAddDialog() {
+        let alertController = UIAlertController(title: "Add a new Weatherpic", message: "", preferredStyle: UIAlertControllerStyle.alert)
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Image URL or blank"
+        }
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Caption"
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil)
+        
+        let createPictureAction = UIAlertAction(title: "Add Photo", style: UIAlertActionStyle.default) { (action) in
+            let photoUrlTextField = alertController.textFields![0]
+            let captionTextField = alertController.textFields![1]
+            
+            let newPicture = Picture(context: self.context)
+            newPicture.pictureUrl = photoUrlTextField.text!
+            newPicture.caption = captionTextField.text!
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            self.updatePictureArray()
+            
+            if self.pictures.count == 1 {
+                self.tableView.reloadData()
+            } else {
+                self.tableView.insertRows(at: [IndexPath(row:0, section:0)], with: UITableViewRowAnimation.top)    }
+        }
+        
+        alertController.addAction(createPictureAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -36,13 +64,22 @@ class PictureTableViewController: UITableViewController {
     
     func updatePictureArray() {
         let request: NSFetchRequest<Picture> = Picture.fetchRequest()
-//        request.sortDescriptors = [NSSortDescriptor(key: "created", ascending: false)]
+        request.sortDescriptors = [NSSortDescriptor(key: "caption", ascending: false)]
         do {
             pictures = try context.fetch(request)
         } catch {
             fatalError("Unresolved Core Data error \(error)")
         }
     }
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        if pictures.count == 0 {
+            super.setEditing(false, animated: animated)
+        } else {
+            super.setEditing(editing, animated: animated)
+        }
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -78,32 +115,23 @@ class PictureTableViewController: UITableViewController {
     }
 
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            context.delete(pictures[indexPath.row])
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            updatePictureArray()
+            
+            if pictures.count == 0 {
+                tableView.reloadData()
+                self.setEditing(false, animated: true)
+            } else {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     
     // MARK: - Navigation
